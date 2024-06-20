07826d991397e6ec5d38f1adfbb0017c7b1203fc
adorahelen <adorahelenmin@gmail.com>
2024-06-20 21:19:45 +0900

    if cannot i go AI

--- test1.py
+++ test1.py
@@ -1,1 +1,9 @@
-1
+print("You know, you dont have skill, ")
+
+reason = input()
+print("That is why you have to study more hard" reason)
+
+a = str(input("haha"))
+b = float(input("haha2"))
+
+print(type(reason))
